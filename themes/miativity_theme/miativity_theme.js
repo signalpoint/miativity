848afb5aa435162be32377205d6492ae7e565ecf
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-15 00:56:36 -0500

    Removed the theme pager implementations since they are now covered by DG core views features.

--- themes/miativity_theme/miativity_theme.js
+++ themes/miativity_theme/miativity_theme.js
@@ -37,37 +37,3 @@ function miativity_theme_info() {
   return theme;
 }
 
-/**
- * Themes a pager next link.
- * @param {Object} variables
- * @return {String}
- */
-function miativity_theme_pager_next(variables) {
-  try {
-    var html;
-    variables.text = 'NEXT &raquo;';
-    variables.attributes.class += ' pager_next';
-    variables.attributes.onclick = 'miativity_gallery_pager_click(this)';
-    html = theme_pager_link(variables);
-    return html;
-  }
-  catch (error) { console.log('miativity_theme_pager_next - ' + error); }
-}
-
-/**
- * Themes a pager previous link.
- * @param {Object} variables
- * @return {String}
- */
-function miativity_theme_pager_previous(variables) {
-  try {
-    var html;
-    variables.text = '&laquo; PREVIOUS';
-    variables.attributes.class += ' pager_prev';
-    variables.attributes.onclick = 'miativity_gallery_pager_click(this)';
-    html = theme_pager_link(variables);
-    return html;
-  }
-  catch (error) { console.log('miativity_theme_pager_previous - ' + error); }
-}
-
